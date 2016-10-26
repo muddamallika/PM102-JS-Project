9f8d87c8cf28db7a7e7d7386b6909ebbd3299003
sra122 <sravan.k.katta@student.fh-kiel.de>
2016-10-26 21:52:00 +0200

    New Events Creation with database is done

--- KIS-Dashboard/Collections/Events_collection.js
+++
@@ -1,5 +0,0 @@
-Tdos = new Mongo.Collection("tdos");
-
-Tdos.schema = new SimpleSchema({
-  label: {type: String},
-  });
--- KIS-Dashboard/client/main.js
+++ KIS-Dashboard/client/main.js
@@ -1,22 +1,5 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-if (Meteor.isClient) {
-    Session.setDefault('counter', 0);
-
-Template.hello.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-  }
-});
-}
-
 
 
