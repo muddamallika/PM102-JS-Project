fc78ec58d7d72c81c12f994a61ae52fb049f45aa
sra122 <sravan.k.katta@student.fh-kiel.de>
2016-12-08 14:33:33 +0100

    Events_modification
    Event functionalities are done

--- KIS-Dashboard/client/Events.js
+++ KIS-Dashboard/client/Events.js
@@ -1,11 +1,18 @@
 
-if(Meteor.isClient) {
+if(Meteor.isClient) {   // Communicating with Client 
 
-Template.todoList.helpers({
+Template.total_events_display.helpers({
   total_events: function() {
     return Total_Events.find();
   }
 });
+
+Template.my_created_events_db.helpers({
+  events: function() {
+    return Events.find();
+  }
+});
+
 }
 if(Meteor.isServer) {
 
