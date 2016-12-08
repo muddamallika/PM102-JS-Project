3665f374e69309f7ec1f6cf3bae33ba97ad92a9f
Mallika Mudda <muddamallika149189@gmail.com>
2016-12-08 20:55:47 +0100

    removed conflicts in main css file

--- KIS-Dashboard/client/DiscussionForum/forumMain.js
+++ KIS-Dashboard/client/DiscussionForum/forumMain.js
@@ -1,14 +1,17 @@
+//Captures the variables from the new discussion form and stores the same.
 if (Meteor.isClient){
   Template.newDisc.events({
    'submit form': function(event){
      event.preventDefault();
      var dTitle = event.target.discTitle.value;
      var dContent = event.target.discContent.value;
+     //Inserts the values discussion title and discussion content variables
      Forum.insert({
        dTitle: dTitle,
        dContent: dContent
      });
-     FlashMessages.sendSuccess("Successfully posted the Discussion");
+     //Sends Flash Messages
+     FlashMessages.sendSuccess("Discussion posted Successfully");
      event.target.discTitle.value="";
      event.target.discContent.value="";
   }
