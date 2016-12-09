7cf0a8c9443eb467f708f40121bb3c2cfe6b735b
Mallika Mudda <muddamallika149189@gmail.com>
2016-12-09 14:22:47 +0100

    Discussion Forum functionality completed, working on ui for Events and Discussion Forum

--- KIS-Dashboard/client/DiscussionForum/forumMain.js
+++ KIS-Dashboard/client/DiscussionForum/forumMain.js
@@ -1,19 +1,8 @@
-//Captures the variables from the new discussion form and stores the same.
+
 if (Meteor.isClient){
-  Template.newDisc.events({
-   'submit form': function(event){
-     event.preventDefault();
-     var dTitle = event.target.discTitle.value;
-     var dContent = event.target.discContent.value;
-     //Inserts the values discussion title and discussion content variables
-     Forum.insert({
-       dTitle: dTitle,
-       dContent: dContent
-     });
-     //Sends Flash Messages
-     FlashMessages.sendSuccess("Discussion posted Successfully");
-     event.target.discTitle.value="";
-     event.target.discContent.value="";
-  }
+  Template.discFetch.helpers({
+    forum: function() {
+      return Forum.find();
+    }
 });
 }
