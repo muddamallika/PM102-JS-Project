d41ac020914f93c1d87eb75c7f87de40aa127842
Mallika Mudda <muddamallika149189@gmail.com>
2016-12-14 22:10:04 +0100

    Developed UI for Event Functionality related pages

--- KIS-Dashboard/client/EventPages/Event_cancel.js
+++ KIS-Dashboard/client/EventPages/Event_cancel.js
@@ -1,19 +1,24 @@
-//
 if(Meteor.isClient){
-//
+//fetches the details of all the events that user have registered for
 Template.canceldb.helpers({
   regis_events: function() {
     return Regis_Events.find().fetch();
   }
 });
-//
+//Cancels the event registration
 Template.cancel_event.events({
 	'submit form':function(event,error){
 		event.preventDefault();
 		var currentid = event.target.cancel.value;
-		Regis_Events.remove({_id: currentid});   // Cancelling the registration for the Event
+    // Cancelling the registration for the Event
+		Regis_Events.remove({_id: currentid});
 		  Router.go('/Events/event');
 	}
-
 });
+//Sends Flash Messages
+FlashMessages.sendSuccess("Cancelled the event successfully");
+//Navigates back to the Event Main Page after event deletion
+Meteor.setTimeout(function () {
+    Router.go('/Events/event');
+}, 5000);
 }
