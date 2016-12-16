9e85ce86ec88c23f7feaeba931e81e8f86bb637e
sra122 <sravan.k.katta@student.fh-kiel.de>
2016-12-16 12:11:46 +0100

    tried to integrate forgot password

--- KIS-Dashboard/client/Login/login.js
+++ KIS-Dashboard/client/Login/login.js
@@ -22,6 +22,31 @@ Template.login.events({
     }
 });
 
+Template.forgotPassword.events({
+    'submit #forgotPasswordForm': function(e, t) {
+        e.preventDefault();
+
+        var forgotPasswordForm = $(e.currentTarget),
+            email = trimInput(forgotPasswordForm.find('#forgotPasswordEmail').val().toLowerCase());
+
+        if (isNotEmpty(email) && isEmail(email)) {
+            Accounts.forgotPassword({email: email}, function(err) {
+                if (err) {
+                    if (err.message === 'User not found [403]') {
+                        Session.set('alert', 'This email does not exist.');
+                    } else {
+                        Session.set('alert', 'We\'re sorry but something went wrong.');
+                    }
+                } else {
+                    Session.set('alert', 'Email Sent. Please check your mailbox to reset your password.');
+
+                }
+            });
+        }
+        return false;
+    }
+});
+
 Template.logout.events({
     'click .logout': function(event){
         event.preventDefault();
@@ -30,4 +55,10 @@ Template.logout.events({
     }
 });
 
+}
+
+if(Meteor.isClient){
+    Meteor.startup(function () {
+    process.env.MAIL_URL="smtp://user%40gmail.com:password@smtp.gmail.com:465"; 
+});
 }
